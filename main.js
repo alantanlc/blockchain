1ccd184202e4a19c7176c33798c27ccae48d91c3
Alan Tan <alanwuha91@gmail.com>
2020-08-08 16:47:12 +0800

    :unicorn: update main.js to add transactions using wallet

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 const { Blockchain, Transaction } = require('./blockchain');
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
+
+const myKey = ec.keyFromPrivate('d207b722d5beeb7256ff1380f29b74538ad8b136ed66ecf6ca39d0bd4f9e5983');
+const myWalletAddress = myKey.getPublic('hex');
 
 let coin = new Blockchain();
 
-coin.createTransaction(new Transaction('address1', 'address2', 100));
-coin.createTransaction(new Transaction('address2', 'address1', 50));
+const tx1 = new Transaction(myWalletAddress, 'public key goes here', 10);
+tx1.signTransaction(myKey);
+coin.addTransaction(tx1);
 
 console.log('\nStarting the miner...');
-coin.minePendingTransactions('alans-address');
+coin.minePendingTransactions(myWalletAddress);
 
-console.log('\nBalance of alan is', coin.getBalanceOfAddress('alans-address'));
-coin.minePendingTransactions('alans-address');
-
-console.log('\nBalance of alan is', coin.getBalanceOfAddress('alans-address'));
+console.log('\nBalance of alan is', coin.getBalanceOfAddress(myWalletAddress));
