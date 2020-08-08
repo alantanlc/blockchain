6f57a5fbacb8977a3d65818c8d3a75bc2861ad76
Alan Tan <alanwuha91@gmail.com>
2020-08-08 14:40:23 +0800

    :racehorse: add isChainValid to check of block chain is valid

--- main.js
+++ main.js
@@ -32,10 +32,39 @@ class Blockchain {
         newBlock.hash = newBlock.calculateHash();
         this.chain.push(newBlock);
     }
+
+    isChainValid() {
+        for (let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i-1];
+
+            if (currentBlock.hash !== currentBlock.calculateHash()) {
+                return false;
+            }
+
+            if (currentBlock.previousHash !== previousBlock.hash) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
 
 let coin = new Blockchain();
 coin.addBlock(new Block(1, "10/08/2020", { amount: 4 }));
 coin.addBlock(new Block(2, "12/08/2020", { amount: 10 }));
 
-console.log(JSON.stringify(coin, null, 4));
+console.log('Is blockchain valid? ' + coin.isChainValid());
+
+coin.chain[1].data = { amount: 100 };
+coin.chain[1].hash = coin.chain[1].calculateHash();
+
+console.log('Is blockchain valid? ' + coin.isChainValid());
+
+coin.chain[2].previousHash = coin.chain[1].hash;
+coin.chain[2].hash = coin.chain[2].calculateHash();
+
+console.log('Is blockchain valid? ' + coin.isChainValid());
+
+// console.log(JSON.stringify(coin, null, 4));
